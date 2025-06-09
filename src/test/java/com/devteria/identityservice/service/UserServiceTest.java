24b86e4dd02679c1a2eae443d08d2b6e871aca5e
vyct1 <thienvy95@gmail.com>
2025-06-09 07:18:49 +0700

    21/uni testing done

---
+++ src/test/java/com/devteria/identityservice/service/UserServiceTest.java
@@ -0,0 +1,124 @@
+package com.devteria.identityservice.service;
+
+import com.devteria.identityservice.dto.request.UserCreationRequest;
+import com.devteria.identityservice.dto.response.UserResponse;
+import com.devteria.identityservice.entity.User;
+import com.devteria.identityservice.entity.Role;
+import com.devteria.identityservice.exception.AppException;
+import com.devteria.identityservice.repository.RoleRepository;
+import com.devteria.identityservice.repository.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.TestPropertySource;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@TestPropertySource("/test.properties")
+public class UserServiceTest {
+    @Autowired
+    private UserService userService;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private RoleRepository roleRepository;
+
+    private UserCreationRequest request;
+    private UserResponse userResponse;
+    private User user;
+    private LocalDate dob;
+    private Role role;
+
+    @BeforeEach
+    void initData() {
+        dob = LocalDate.of(1990, 1, 1);
+
+        request = UserCreationRequest.builder()
+                .username("john")
+                .firstName("John")
+                .lastName("Doe")
+                .password("12345678")
+                .dob(dob)
+                .build();
+
+        userResponse = UserResponse.builder()
+                .id("cf0600f538b3")
+                .username("john")
+                .firstName("John")
+                .lastName("Doe")
+                .dob(dob)
+                .build();
+
+        user = User.builder()
+                .id("cf0600f538b3")
+                .username("john")
+                .firstName("John")
+                .lastName("Doe")
+                .dob(dob)
+                .build();
+
+        role = Role.builder()
+                .name("User")
+                .description("User role")
+                .build();
+    }
+
+    @Test
+    void createUser_validRequest_success() {
+        // GIVEN
+        when(userRepository.existsByUsername(anyString())).thenReturn(false);
+        when(userRepository.save(any())).thenReturn(user);
+        when(roleRepository.findById(com.devteria.identityservice.enums.Role.USER.name())).thenReturn(Optional.ofNullable(role));
+
+        // WHEN
+        var response = userService.createUser(request);
+        // THEN
+
+        Assertions.assertThat(response.getId()).isEqualTo("cf0600f538b3");
+        Assertions.assertThat(response.getUsername()).isEqualTo("john");
+    }
+
+    @Test
+    void createUser_userExisted_fail() {
+        // GIVEN
+        when(userRepository.existsByUsername(anyString())).thenReturn(true);
+
+        // WHEN
+        var exception = assertThrows(AppException.class, () -> userService.createUser(request));
+
+        // THEN
+        Assertions.assertThat(exception.getErrorCode().getCode()).isEqualTo(1002);
+    }
+
+    @Test
+    @WithMockUser(username = "john")
+    void getMyInfo_valid_success() {
+        when(userRepository.findByUsername(anyString())).thenReturn(Optional.of(user));
+
+        var response = userService.getMyInfo();
+
+        Assertions.assertThat(response.getUsername()).isEqualTo(user.getUsername());
+        Assertions.assertThat(response.getId()).isEqualTo(user.getId());
+    }
+
+    @Test
+    @WithMockUser(username = "doe")
+    void getMyInfo_userNotFound_error() {
+        when(userRepository.findByUsername(anyString())).thenReturn(Optional.ofNullable(null));
+        var exception = assertThrows(AppException.class, () -> userService.getMyInfo());
+        Assertions.assertThat(exception.getErrorCode().getCode()).isEqualTo(1005);
+    }
+}
